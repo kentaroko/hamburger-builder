a9c03a24313f9d1ecfb6b5f4836d010bedfcf78f
Ken Chen <ken.chen@taroko.io>
2019-05-15 18:00:23 +0800

    add another test suite and have beforeEach

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,8 +7,16 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
   it('should render two <NavigatioItem /> elements if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);;
+  });
+
+  it('should render three <NavigatioItem /> elements if authenticated', () => {
+    wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);;
   });
 });
